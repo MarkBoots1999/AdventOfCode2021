96c5e6ce914be1569db320f1742dd7dcd304d3ef
Mark Boots <79016690+MarkBoots1999@users.noreply.github.com>
2021-12-11 01:18:10 +0100

    Update 10.py

--- Day10/10.py
+++ Day10/10.py
@@ -11,21 +11,41 @@ match_score = {")": 3,
                "}": 1197,
                ">": 25137}
 
+closing_score = {")": 1,
+                 "]": 2,
+                 "}": 3,
+                 ">": 4}
 
+# Score part 1
 score = 0
+# For part 2
+score_array = []
 
 for line in data:
-    stack = [] # Ignore incomplete lines by resetting them every iteration
+    # Ignore incomplete lines by resetting them every iteration
+    stack = []
+     
     for sign in line:
-        #print(stack)
         if sign in "([{<":
             stack.append(sign)
         else:
-            if sign == allowed_match[stack[-1]]: #stack == None or 
-                #print(f"closed {allowed_match[stack[-1]]}")
+            if sign == allowed_match[stack[-1]]:
                 stack.pop()
-            elif stack == None or sign != allowed_match[stack[-1]]:
-                #print(f"{sign} error")    
+            elif stack == [] or sign != allowed_match[stack[-1]]:
                 score += match_score[sign]
-                break # Break after first error in line
-print(score)
+                stack.clear()
+                # Break after first encountered error in line
+                break 
+    
+    # Part 2        
+    score_2 = 0    
+    
+    if stack:
+        for element in reversed(stack):
+            score_2 = 5 * score_2 + closing_score[ allowed_match[element] ]
+        
+        score_array.append(score_2)
+        
+middle_value = sorted(score_array)[len(score_array)//2]
+print(f"Score Part 1 {score}")
+print(f"Score Part 2 {middle_value}")
